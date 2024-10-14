8f02e9ff7a9d94eaf58d677d7ac05ff688755983
Philipe Dias Mattos <philipedm@gmail.com>
2024-10-13 21:06:28 -0300

    Create API
    Create API and publish

---
+++ Controllers/StatusHttpController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_StatusHttp.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class StatusHttpController : ControllerBase
+    {
+        [HttpGet(Name = "GetStatusHTTP")]
+        public StatusHttp GetStatus(int cod)
+        {
+            var name = Enum.GetName(typeof(EnumStatusHttp), cod);
+            EnumStatusHttp enumStatusHttp = (EnumStatusHttp)cod;
+            var statusReturn = new StatusHttp {
+                Status = cod,
+                Response = name,
+                Description = name != null ? enumStatusHttp.GetEnumDescription() : "Código de status inexistente"
+            };
+
+            return statusReturn;
+        }
+    }
+}
---
+++ StatusHttp.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace API_StatusHttp
+{
+    public class StatusHttp
+    {
+        public int Status { get; set; }
+        public string? Response { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public static class Extension
+    {
+        public static string GetEnumDescription(this Enum enumValue)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                return attribute.Description;
+            }
+            throw new ArgumentException("Item not found.", nameof(enumValue));
+        }
+    }
+}
